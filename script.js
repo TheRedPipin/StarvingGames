836a3247b00a684e33c87d608e8e89d3660e4e95
TheRedPipin <61920753+TheRedPipin@users.noreply.github.com>
2024-12-13 15:17:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-document.write("Hello World")
+
+//class
+class mother {
+    contestent(name,age,gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.district = district;
+        this.health = health
+        this.health = 100
+    }
+}
+
+
+
+//objects
+const contestent1 = new mother({name:"peniscat", age:"30", gender:"M", district:2});
+const contestent2 = new mother({name:"Jimmy", age:"12", gender:"M", district:1});
+const contestent3 = new mother({name:"Kaylee", age:"45", gender:"F", district:1});
+const contestent4 = new mother({name:"Eden", age:"16", gender:"F", district:2});
+console.log(contestent1, contestent2, contestent3, contestent4);
+
