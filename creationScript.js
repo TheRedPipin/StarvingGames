ce5df4ff85dc519de8491af30646877b1fa7dd27
TheRedPipin <61920753+TheRedPipin@users.noreply.github.com>
2024-12-18 15:33:04 +0000

    Updated Landing Page
    Added the saving of District Names and a input for women (there people now)

--- creationScript.js
+++ creationScript.js
@@ -2,7 +2,16 @@
 //class
 let contestentList = [];
 let stats = {};
-let background = 0
+let background = 0;
+let tempNames = [];
+let tempAge = [];
+let lastValue = 0;
+
+for(let i = 0; i < 12; i++){
+    tempNames.push(["",""]);
+    tempAge.push(["", ""]);
+    
+}
 
 class mother {
     constructor({name,age,gender,district,stats}) { 
@@ -10,70 +19,48 @@ class mother {
         this.age = age;
         this.gender = gender;
         this.district = district;
-        this.stats = stats
+        this.stats = stats;
     }
 }
+
 
 function end(){
     //ask for contestent names
     let count = 0;
-    let contestentName = ""
-    while (count < 24) {
-        //contestentName = prompt("Enter contestent" +count);
-        contestentName = count.toString()
-        count +=1;
-        contestentList.push(contestentName);
+    while (count < 12) {
+        contestentList.push(tempNames[count][0]);
+        contestentList.push(tempNames[count][1]);
         //strength is a key
         stats["strength"] = 0;
         stats["speed"] = 0;
         stats["intelligence"] = 0;
         stats["charisma"] = 0;
         console.log(stats);
+        count +=1;
     }
 }
+//calls in html creationIndex with alll the indexes (trust)
+function districtChoice(districtNumber){
+    //temp name
+    tempNames[lastValue][0] = document.getElementById("tempNameInputMale").value;
+    document.getElementById("tempNameInputMale").value = tempNames[districtNumber][0];
+
+    tempNames[lastValue][1] = document.getElementById("tempNameInputFemale").value;
+    document.getElementById("tempNameInputFemale").value = tempNames[districtNumber][1];
+    //temp age
+    tempAge[lastValue][0] = document.getElementById("tempAgeInputMale").value;
+    document.getElementById("tempAgeInputMale").value = tempAge[districtNumber][0];
+    tempAge[lastValue][1] = document.getElementById("tempAgeInputFemale").value;
+    document.getElementById("tempAgeInputFemale").value = tempAge[districtNumber][1];
+    //reset 
+    lastValue = districtNumber;
+}
+
+
 
 //switch page
 function switchARoo(){
+    end()
     localStorage.setItem("names" , JSON.stringify(contestentList))
     window.location.href = "./simulationIndex.html";
 }
-end()
-/*
-    //objects
-    //district 1
-    const contestent1 = new mother({name:contestentList[0], age:"30", gender:"M", district:1, stats:4});
-    const contestent2 = new mother({name:contestentList[1], age:"45", gender:"F", district:1});
-    //district 2
-    const contestent3 = new mother({name:contestentList[2], age:"12", gender:"M", district:2});
-    const contestent4 = new mother({name:contestentList[3], age:"16", gender:"F", district:2});
-    //district 3
-    const contestent5 = new mother({name:contestentList[4], age:"32", gender:"M", district:3});
-    const contestent6 = new mother({name:contestentList[5], age:"41", gender:"F", district:3});
-    //district 4
-    const contestent7 = new mother({name:contestentList[6], age:"78", gender:"M", district:4});
-    const contestent8 = new mother({name:contestentList[7], age:"18", gender:"F", district:4});
-    //district 5
-    const contestent9 = new mother({name:contestentList[8], age:"23", gender:"M", district:5});
-    const contestent10 = new mother({name:contestentList[9], age:"34", gender:"F", district:5});
-    //district 6
-    const contestent11 = new mother({name:contestentList[10], age:"17", gender:"M", district:6});
-    const contestent12 = new mother({name:contestentList[11], age:"42", gender:"F", district:6});
-    //district 7
-    const contestent13 = new mother({name:contestentList[12], age:"14", gender:"M", district:7});
-    const contestent14 = new mother({name:contestentList[13], age:"56", gender:"F", district:7});
-    //district 8
-    const contestent15 = new mother({name:contestentList[14], age:"16", gender:"M", district:8});
-    const contestent16 = new mother({name:contestentList[15], age:"27", gender:"F", district:8});
-    //district 9
-    const contestent17 = new mother({name:contestentList[16], age:"29", gender:"M", district:9});
-    const contestent18 = new mother({name:contestentList[17], age:"21", gender:"F", district:9});
-    //district 10
-    const contestent19 = new mother({name:contestentList[18], age:"11", gender:"M", district:10});
-    const contestent20 = new mother({name:contestentList[19], age:"27", gender:"F", district:10});
-    //district 11
-    const contestent21 = new mother({name:contestentList[20], age:"17", gender:"M", district:11});
-    const contestent22 = new mother({name:contestentList[21], age:"36", gender:"F", district:11});
-    //district 12
-    const contestent23 = new mother({name:contestentList[22], age:"15", gender:"M", district:12});
-    const contestent24 = new mother({name:contestentList[23], age:"14", gender:"F", district:12});
-*/
